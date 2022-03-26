df82aee214871b6f2cde59ff42ad1111f1c678b8
nvtmjfan <nvtmjfan@gmail.com>
2022-03-26 21:18:06 +0700

    write overload method bindHTML

--- script.js
+++ script.js
@@ -136,7 +136,7 @@ function bodyOnload() {
         }
     }, 10);
 
-    includeHTML();
+    bindHtml(summaryExp);
 }
 
 function viewMedAidAward() {
@@ -146,30 +146,22 @@ function viewMedAidAward() {
     medAidAward.setAttribute('src', src);
 }
 
-function includeHTML() {
-    var z, i, elmnt, file, xhttp;
-    /*loop through a collection of all HTML elements:*/
-    z = document.getElementsByTagName("*");
-    for (i = 0; i < z.length; i++) {
-        elmnt = z[i];
-        /*search for elements with a certain atrribute:*/
-        file = elmnt.getAttribute("w3-include-html");
-        if (file) {
-            /*make an HTTP request using the attribute value as the file name:*/
-            xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function() {
-                if (this.readyState == 4) {
-                    if (this.status == 200) { elmnt.innerHTML = this.responseText; }
-                    if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
-                    /*remove the attribute, and call this function once more:*/
-                    elmnt.removeAttribute("w3-include-html");
-                    includeHTML();
-                }
-            }
-            xhttp.open("GET", file, true);
-            xhttp.send();
-            /*exit the function:*/
-            return;
+/**
+ * Bind html form file source to element target
+ * @param {*} elTarge element target
+ * @param {*} fileSource name of file source 
+ * @returns void
+ */
+function bindHtml(elTarget, fileSource) {
+    if (!fileSource) return bindHtml(elTarget, elTarget.getAttribute('src-html'))
+        /*make an HTTP request using the attribute value as the file name:*/
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+            if (this.status == 200) { elTarget.innerHTML = this.responseText; }
+            if (this.status == 404) { elTarget.innerHTML = "Page not found."; }
         }
     }
+    xhttp.open("GET", fileSource, true);
+    xhttp.send();
 }
